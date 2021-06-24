5be5a76dd9516b8dde761996122468eea49fc861
Michel Bechelani <michel@bechelani.com>
2021-06-24 00:45:13 -0700

    update terraform common dns
    - ignore number_of_record_sets

--- terraform/common/dns.tf
+++ terraform/common/dns.tf
@@ -3,5 +3,11 @@ resource "azurerm_dns_zone" "public" {
   name                = "whatisthecurrentsprint.com"
   resource_group_name = azurerm_resource_group.common.name
 
+  # Ignore number_of_record_sets as they could be updated using
+  # other modules
+  lifecycle {
+    ignore_changes = [number_of_record_sets]
+  }
+
   tags = var.tags
 }
